9cd7357a789870d33ba4193330e4d1b02932544e
vorxsaken <jhonyevans6326@gmail.com>
2023-02-17 21:17:56 +0700

    feat: next auth global default font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
+import { SessionProvider } from "next-auth/react"
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import type { AppProps } from 'next/app'
+import type { Session } from "next-auth"
+
+import { Ubuntu } from "@next/font/google"
+
+const ubuntu = Ubuntu({weight: '400', subsets: ['cyrillic']})
+
+
+export default function App({ Component, pageProps : { session, ...pageProps } }: AppProps<{ session: Session }>) {
+  return (
+    <SessionProvider session={session}>
+      <div className={ubuntu.className}>
+        <Component {...pageProps} />
+      </div>
+    </SessionProvider>
+  )
 }
