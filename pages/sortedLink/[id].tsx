fb8c39184fb19405f45ae453839202964426dd80
vorxsaken <jhonyevans6326@gmail.com>
2023-02-20 23:25:00 +0700

    feat: download qrcode

--- pages/sortedLink/[id].tsx
+++ pages/sortedLink/[id].tsx
@@ -1,7 +1,7 @@
 import Button from "@/components/Button";
 import { PrismaClient } from "@prisma/client";
 import Layout from "@/components/Layout";
-import { charm } from "@/utils";
+import { charm, currentDate } from "@/utils";
 import { MdOutlineContentCopy, MdCheckCircle } from "react-icons/md";
 import { useState } from "react"
 import {QRCode} from 'react-qrcode-logo'
@@ -17,6 +17,15 @@ export default function sortedLink({ alias }: { alias: string }) {
         setTimeout(() => {
             setIsCopy((v) => !v)
         }, 3000)
+    }
+
+    const download = () => {
+        const qrcode = document.getElementById('qrcode') as HTMLCanvasElement;
+        const link = document.createElement('a') as HTMLAnchorElement;
+
+        link.download = `${ currentDate() } shorten qrcode.png`;
+        link.href = qrcode?.toDataURL();
+        link.click();
     }
 
     return (
@@ -49,9 +58,13 @@ export default function sortedLink({ alias }: { alias: string }) {
                     </div>
                     <div className="qrcodeContainer">
                         <div className="qrcode">
-                            <QRCode value={shortentextValue} size={250} />
+                            <QRCode 
+                            id="qrcode"
+                            value={shortentextValue} 
+                            bgColor="rgba(255, 255, 255, 0%)" 
+                            size={250} />
                         </div>
-                        <Button>Download</Button>
+                        <Button onClick={() => download()}>Download</Button>
                     </div>
                 </div>
             </div>
