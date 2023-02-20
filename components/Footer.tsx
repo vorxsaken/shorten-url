625139ba185027094145475172203516d3b3ad28
vorxsaken <jhonyevans6326@gmail.com>
2023-02-20 23:30:03 +0700

    feat: footer component

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import styles from "./Footer.module.css"
+import { AiFillGithub, AiFillInstagram } from 'react-icons/ai'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className={styles.container}>
+      <div className={styles.body}>
+        <div className={styles.icons}>
+          <a href="https://github.com/vorxsaken"
+            target="_blank"
+            rel="noopener noreferrer">
+            <AiFillGithub />
+          </a>
+          <a
+            href="https://www.instagram.com/vorxsaken/"
+            target="_blank"
+            rel="noopener noreferrer">
+            <AiFillInstagram />
+          </a>
+        </div>
+        <div className={styles.content}>
+          <p>
+            Fueled with NextJS. Source code available in &nbsp;
+            <a
+              href="https://github.com/vorxsaken/shorten-url"
+              target="_blank"
+              rel="noopener noreferrer">
+                github
+            </a>
+          </p>
+        </div>
+        <span className={styles.ikan}>Vorxsaken &#169; 2023</span>
+      </div>
+    </div>
   )
 }
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import NavBar from "./NavBar"
+import Footer from "./Footer"
 import type { ReactNode } from "react"
 
 export default function Layout({ children }: { children: ReactNode }) {
@@ -6,6 +7,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         <>
             <NavBar />
             <div>{children}</div>
+            <Footer />
         </>
     )
 }
