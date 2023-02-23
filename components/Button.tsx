a42590379016d239862829253c1570db10c84ca3
vorxsaken <jhonyevans6326@gmail.com>
2023-02-23 12:09:18 +0700

    chore: add loading

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,20 +1,26 @@
 import { MouseEventHandler, ReactNode } from 'react';
 import styles from './Button.module.css';
+import loadingGearWhite from '../assets/Gear-0.2s-200px white.svg'
+import Image from 'next/image';
 
 interface Button {
     children: ReactNode,
     onClick?: MouseEventHandler,
-    icon?: boolean
+    icon?: boolean,
+    loading?: boolean
 }
 
-export default function Button({ children, onClick, icon }: Button) {
+export default function Button({ children, onClick, icon, loading }: Button) {
     return (
         <div onClick={onClick} className={icon ? styles.smallButton : styles.button}>
-            {children}
+            {loading ? (
+                <Image src={loadingGearWhite} alt="loading ..." width={30} height={30} />
+            ) : children }
         </div>
     )
 }
 
 Button.defaultProps = {
-    icon: false
+    icon: false,
+    loading: false
 }
