5f1c0437ce8f47a88d5e07a71eb4016ed48b0476
vorxsaken <jhonyevans6326@gmail.com>
2023-02-23 12:10:23 +0700

    feat: api for delete link

---
+++ pages/api/deleteLink/[id].ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { id } = req.query;
+    console.log(id);
+    const prisma = new PrismaClient();
+    const session = await getServerSession(req, res, authOptions);
+
+    if (session) {
+        try {
+            await prisma.shorten.delete({
+                where: {
+                    id: id as string
+                }
+            })
+
+            return res.status(200).send({ id: id })
+
+        } catch (error) {
+
+            return res.status(500).send({message: 'theres something wrong'})
+
+        }
+    }
+
+    res.status(511).send({message: "authentication required"});
+
+}
