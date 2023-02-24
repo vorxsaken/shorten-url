4b73a803d357b3ff07f35ecade1f500b5778d4ef
vorxsaken <jhonyevans6326@gmail.com>
2023-02-25 01:04:20 +0700

    fix: different session naming

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,8 @@ import { NextResponse } from "next/server";
 export const config = { matcher: '/userLinks'};
 
 export function middleware(req: NextRequest) {
-    const session = req.cookies.get('next-auth.session-token');
-
+    const session = req.cookies.get('__Secure-next-auth.session-token');
+    
     if(session) {
         return NextResponse.next();
     }
