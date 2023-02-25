8d136d112a1c3ea85c614349f77ca543a6f7681a
vorxsaken <jhonyevans6326@gmail.com>
2023-02-25 18:58:06 +0700

    feat: count number of people visiting shorten page

--- pages/api/[...slug].ts
+++ pages/api/[...slug].ts
@@ -4,7 +4,7 @@ import { PrismaClient } from "@prisma/client";
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { slug } = req.query;
     const prisma = new PrismaClient();
-    
+
     const getUrl = await prisma.shorten.findMany({
         where: {
             alias: slug?.at(0)
@@ -12,6 +12,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     if (getUrl.length > 0) {
+
+        await prisma.shorten.updateMany({
+            where: {
+                alias: slug?.at(0)
+            },
+            data: {
+                counter: {
+                    increment: 1
+                }
+            }
+        })
 
         res.writeHead(301, {
             "Location": getUrl[0].url
